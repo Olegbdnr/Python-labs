19b15fde25a744481fbd674d1223043d009efeb0
Olegbdnr <oleg.br.1304@gmail.com>
2022-06-09 01:56:16 +0300

    Added lab 10

---
+++ lab_10/main.py
@@ -0,0 +1,25 @@
+from LinkedList import ListNode
+from LinkedList import LinkedList
+
+
+def main():
+    node1 = ListNode(4, 2)
+    node2 = ListNode(5, 2)
+    node3 = ListNode(9, 2)
+    node4 = ListNode(8, 2)
+    list = LinkedList()
+    list.add(node1)
+    list.add(node2)
+    list.add(node3)
+    list.add(node4)
+    list.get_all()
+    print("Length =", list.length())
+    list.search(8, 2)
+    list.remove_by_id(1)
+    list.get_all()
+    print(list.distance_between_first_and_last())
+
+
+if __name__ == '__main__':
+    main()
+
